026b798891c3c8fce30dafedb64877355c26c998
nehasharma2998 <nehasharma29@outlook.com>
2019-01-19 20:33:10 -0500

    log in with username now

--- groupfund/users/forms.py
+++ groupfund/users/forms.py
@@ -8,7 +8,7 @@ from flask_login import current_user
 from groupfund.models import User
 
 class LoginForm(FlaskForm):
-    email = StringField('Email',validators=[DataRequired(),Email()])
+    username = StringField('Username',validators=[DataRequired()])
     password = PasswordField('Password',validators=[DataRequired()])
     submit = SubmitField('Log In')
 
