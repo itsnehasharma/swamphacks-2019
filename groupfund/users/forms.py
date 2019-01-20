43c62e11b248d25636a05b810bbf31194e9315d7
nehasharma2998 <nehasharma29@outlook.com>
2019-01-19 21:55:44 -0500

    bootstrapping

--- groupfund/users/forms.py
+++ groupfund/users/forms.py
@@ -14,7 +14,7 @@ class LoginForm(FlaskForm):
 
 class RegistrationForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired(),Email()])
-    username = StringField('UserName',validators=[DataRequired()])
+    username = StringField('Username',validators=[DataRequired()])
     password = PasswordField('Password',validators=[DataRequired(),EqualTo('pass_confirm',message='Passwords must match!')])
     pass_confirm = PasswordField('Confirm Password',validators=[DataRequired()])
     submit = SubmitField('Register!')
